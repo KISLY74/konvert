0b6d7751608c51a2ae8d41e197e1cf47982586ab
KISLY74 <kisly.nikita@mail.ru>
2023-01-17 15:25:05 +0300

    feat: add header and materials components

---
+++ src/pages/Learning.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+import Materials from "../components/Materials"
+
+const Learning = () => {
+  return <>
+    <Header />
+    <Materials />
+  </>
+}
+
+export default Learning
