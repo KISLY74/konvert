aaa9be3a96e2b453052feb7a1260bbbe0ac03438
KISLY74 <kisly.nikita@mail.ru>
2023-01-21 17:48:09 +0300

    fix: change component for preview pdf

--- src/pages/DocumentPage.js
+++ src/pages/DocumentPage.js
@@ -1,7 +1,7 @@
-import { DocumentView } from "../components/DocumentView";
+import PdfPreview from "../components/PdfPreview"
 
 export default function DocumentPage() {
   return <div className="d-flex justify-content-center align-items-center flex-column" style={{ height: '100vh' }}>
-    <DocumentView />
+    <PdfPreview />
   </div>
 }
